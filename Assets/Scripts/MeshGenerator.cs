fe440b5013929d7d916fdcb7a9d81ef24fde0b65
linzexin <linzexinmasterchief@gmail.com>
2021-05-31 03:48:47 +0800

    made a weird pyramid

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -35,7 +35,9 @@ public class MeshGenerator : MonoBehaviour
         triangles = new int[]
         {
             0, 2, 1,
-            3, 1, 2
+            3, 1, 2,
+            0, 1, 3,
+            3, 2, 0
         };
     }
 
